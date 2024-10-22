84c20502fd0bd6a07764b60966f34d88c4a79da9
vertusd <61446504+vertusdster@users.noreply.github.com>
2024-10-22 16:24:48 +0000

    update parameters

--- main.py
+++ main.py
@@ -367,8 +367,11 @@ class PageResource(_DAVResource):
 config = {
     "provider_mapping": {"/": MangaDAVProvider()},
     "simple_dc": {"user_mapping": {"*": True}},  # 允许所有用户访问
-    "verbose": 3,  # 输出更多详细日志,
-    "max_propfind_depth": 1,  # 限制 PROPFIND 的最大深度为 1
+    "verbose": 2,  # 输出更多详细日志,
+    "max_propfind_depth": 0,  # 限制 PROPFIND 的最大深度为 1
+    "emulate_win32_lastmod": True,  # 解决某些客户端依赖的 Windows 兼容性问题
+    "re_encode_path_info": True,  # 处理 URL 中编码不一致的问题
+}
 }
 
 # 启动 WebDAV 服务器
